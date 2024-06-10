7df8a31c3dd86fc10947900d074b910bf28dd658
sandao666 <2356610010@qq.com>
2024-06-10 19:55:17 +0800

    Site updated: 2024-06-10 19:55:17

--- js/foot.js
+++ js/foot.js
@@ -1,17 +1,17 @@
-// 动态心跳，更改自己的名称
+// 动态心跳
 $(document).ready(function(e){
-    $('.copyright').html('©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By Sand4o');
+    $('.copyright').html('©2024 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By Sand4o');
 })
-​
+
 $(document).ready(function(e){
     show_date_time();
 })
-​
-//本站运行时间，更改自己建立站点的时间
+
+//本站运行时间
 function show_date_time(){
-$('.framework-info').html('小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span>');
+$('.framework-info').html('本站已运行<span id="span_dt_dt" style="color: #fff;"></span>');
 window.setTimeout("show_date_time()", 1000);
-BirthDay=new Date("6/7/2024 0:0:0");
+BirthDay=new Date("6/8/2024 0:0:0");
 today=new Date();
 timeold=(today.getTime()-BirthDay.getTime());
 sectimeold=timeold/1000
